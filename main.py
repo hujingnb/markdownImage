b4a7dbde463ee8077d226de50b72c6ac547102f0
hujing <hujingnb@qq.com>
2019-12-17 22:42:28 +0800

    添加方法抽象

--- main.py
+++ main.py
@@ -11,6 +11,48 @@ import os
 import sys
 
 
+def dispose_image_to_base64(file_path, content, is_use_id):
+    """
+    将内容中的图片标签转换成base64编码后返回
+    :param file_path: 文件路径
+    :param content: 字符串内容
+    :param is_use_id: 是否使用id替换内容
+    :return:
+    """
+    # 正则匹配图片tag
+    img_tag_pattern = re.compile(r'!\[[\w\d]*\]\([^\(\)]*\)')
+    # 正则匹配图片标签中的图片url
+    img_url_pattern = re.compile(r'\(([^\) ]*)')
+    search = img_tag_pattern.findall(content)
+    # 不存在图片标签
+    if not search:
+        return content, dict()
+    img_id_map = dict()
+    # 遍历处理每一个图片内容
+    for each in search:
+        # 拿到图片url
+        url = img_url_pattern.search(each)
+        # 若没有匹配到, 跳过
+        if not url:
+            continue
+        url = url.group(1)
+        # 若路径是相对路径,将路径与md文件目录拼接
+        if not os.path.isabs(url):
+            url = os.path.join(os.path.dirname(file_path), url)
+        # 获取图片的base64
+        img_base64 = ImageBase64.base64_img(url)
+        # 将图片base64直接放到标签中
+        if not is_use_id:
+            # 将base64转到图片标签中
+            content = content.replace(url, img_base64)
+        # 将图片标签中存放id, id放到文件最后
+        else:
+            img_id = str(uuid.uuid1())
+            img_id_map[img_id] = img_base64
+            content = content.replace('(' + url + ')', '[' + img_id + ']')
+    return content,img_id_map
+
+
 def change_md_image(in_file_path, out_file_path, is_use_id=False, encoding='utf-8'):
     """
     将markdown文件中的图片路径修改为base64编码
@@ -20,43 +62,14 @@ def change_md_image(in_file_path, out_file_path, is_use_id=False, encoding='utf-
     :param encoding 文件编码, 默认utf8
     :rtype: object
     """
-    # 正则匹配图片tag
-    img_tag_pattern = re.compile(r'!\[[\w\d]*\]\([^\(\)]*\)')
-    # 正则匹配图片标签中的图片url
-    img_url_pattern = re.compile(r'\(([^\) ]*)')
     # 保存图片id的map,在最后将id写出到文件
     img_id_map = dict()
     with open(out_file_path, 'w+', encoding=encoding) as out_file, \
             open(in_file_path, 'r', encoding=encoding) as in_file:
         # 读取输入文件
         for line in in_file.readlines():
-            search = img_tag_pattern.findall(line)
-            # 不存在图片标签
-            if not search:
-                out_file.write(line)
-                continue
-            # 遍历处理每一个图片内容
-            for each in search:
-                # 拿到图片url
-                url = img_url_pattern.search(each)
-                # 若没有匹配到, 跳过
-                if not url:
-                    continue
-                url = url.group(1)
-                # 若路径是相对路径,将路径与md文件目录拼接
-                if not os.path.isabs(url):
-                    url = os.path.join(os.path.dirname(in_file_path), url)
-                # 获取图片的base64
-                img_base64 = ImageBase64.base64_img(url)
-                # 将图片base64直接放到标签中
-                if not is_use_id:
-                    # 将base64转到图片标签中
-                    line = line.replace(url, img_base64)
-                # 将图片标签中存放id, id放到文件最后
-                else:
-                    img_id = str(uuid.uuid1())
-                    img_id_map[img_id] = img_base64
-                    line = line.replace('(' + url + ')', '[' + img_id + ']')
+            line, this_img_id_map = dispose_image_to_base64(in_file_path, line, is_use_id)
+            img_id_map.update(this_img_id_map)
             # 写入输出文件
             out_file.write(line)
         # 遍历完成, 将id写入文件最后
