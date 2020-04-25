d2ecfcfb86b0c22ad0848683a1a7eae567c2ebb3
hujingnb <hujingnb@qq.com>
2020-04-25 19:02:31 +0800

    将参数的处理单独拿到一个类中

--- main.py
+++ main.py
@@ -6,23 +6,21 @@ Created on 2019/5/18 19:12
 from src.ImageBase64 import ImageBase64
 import re
 import uuid
-import getopt
 import os
-import sys
+from src.Args import arg
 
 
-def dispose_image_to_base64(file_path, content, is_use_id):
+def dispose_image_to_base64(file_path, content):
     """
     将内容中的图片标签转换成base64编码后返回
     :param file_path: 文件路径
     :param content: 字符串内容
-    :param is_use_id: 是否使用id替换内容
     :return:
     """
     # 正则匹配图片tag
-    img_tag_pattern = re.compile(r'!\[[^\]]*\]\([^\(\)]*\)')
+    img_tag_pattern = re.compile(r'!\[[^\]]*\]\([^()]*\)')
     # 正则匹配图片标签中的图片url
-    img_url_pattern = re.compile(r'\(([^\)]*)')
+    img_url_pattern = re.compile(r'\(([^)]*)')
     search = img_tag_pattern.findall(content)
     # 不存在图片标签
     if not search:
@@ -44,7 +42,7 @@ def dispose_image_to_base64(file_path, content, is_use_id):
         # 获取图片的base64
         img_base64 = ImageBase64.base64_img(img_url)
         # 将图片base64直接放到标签中
-        if not is_use_id:
+        if not arg.use_id:
             # 将base64转到图片标签中
             content = content.replace(img_url_word, img_base64)
         # 将图片标签中存放id, id放到文件最后
@@ -55,95 +53,33 @@ def dispose_image_to_base64(file_path, content, is_use_id):
     return content, img_id_map
 
 
-def change_md_image(in_file_path, out_file_path, is_use_id=False, encoding='utf-8'):
+def change_md_image():
     """
     将markdown文件中的图片路径修改为base64编码
-    :param in_file_path 输入markdown文件路径
-    :param out_file_path 输出markdown文件路径
-    :param is_use_id 是否在markdown文件中使用id, 存放到文件最后
-    :param encoding 文件编码, 默认utf8
     :rtype: object
     """
     # 保存图片id的map,在最后将id写出到文件
     img_id_map = dict()
-    with open(out_file_path, 'w+', encoding=encoding) as out_file, \
-            open(in_file_path, 'r', encoding=encoding) as in_file:
+    with open(arg.out_file, 'w+', encoding=arg.encoding) as out_file, \
+            open(arg.in_file, 'r', encoding=arg.encoding) as in_file:
         # 读取输入文件
         for line in in_file.readlines():
-            line, this_img_id_map = dispose_image_to_base64(in_file_path, line, is_use_id)
+            line, this_img_id_map = dispose_image_to_base64(arg.in_file, line)
             img_id_map.update(this_img_id_map)
             # 写入输出文件
             out_file.write(line)
         # 遍历完成, 将id写入文件最后
         for key, value in img_id_map.items():
-            out_file.write(os.linesep + '[' + key + ']:' + value+ os.linesep)
-
-
-def print_help():
-    """
-    输出帮助文档
-    :rtype: object
-    """
-    print("""
-参数如下: 
-    -h/--help: 显示帮助文档
-    -i/--in: md文件路径, 必填
-    -o/--out: md输出文件路径, 默认 原文件名.md2.md
-    -d/--id: 是否使用id表示, 默认不适用, 0(不使用),1(使用)
-    -e/--encoding: 文件编码, 默认utf-8
-        制作者: 靖哥哥
-    """)
-
-
-def read_args():
-    """
-    读取命令行参数
-    :return 参数map
-    """
-    # 读取参数
-    result = dict()
-    tmp, args = getopt.getopt(sys.argv[1:], 'i:o:d:e:h', ['encoding=', 'out=', 'in=', 'id=', 'help'])
-    # 读取参数
-    for comm in tmp:
-        # 帮助文档
-        if comm[0] == '-h' or comm[0] == '--help':
-            result['help'] = True
-        elif comm[0] == '-i' or comm[0] == '--in':
-            result['in'] = comm[1]
-        elif comm[0] == '-o' or comm[0] == '--out':
-            result['out'] = comm[1]
-        elif comm[0] == '-d' or comm[0] == '--id':
-            result['id'] = comm[1]
-        elif comm[0] == '-e' or comm[0] == '--encoding':
-            result['encoding'] = comm[1]
-    return result
+            out_file.write(os.linesep + '[' + key + ']:' + value + os.linesep)
 
 
 if __name__ == '__main__':
-    # Python2显示升级提示
-    if sys.version < '3':
-        print("""
-抱歉, 暂不支持Python2
-    请升级Python版本后重试
-        """)
-        exit()
-    # 读取参数
-    params = None
-    try:
-        params = read_args()
-    except getopt.GetoptError as e:
-        # 出错了, 显示提示文档
-        print_help()
-        exit()
-    # 是否显示帮助文档
-    if not params or params.get('help') or not params.get('in'):
-        print_help()
-        exit()
-    # 设置默认参数
-    params['out'] = params['out'] if params.get('out') else str(params['in'])+'2.md'
-    params['encoding'] = params['encoding'] if params.get('encoding') else 'utf-8'
-    params['id'] = params['id'] if params.get('id') else False
+    # 检查版本号
+    arg.check_version()
+    # 判断是否输出帮助文档
+    if arg.need_print_help():
+        arg.print_help()
     # 处理文件
-    change_md_image(params['in'], params['out'], params['id'], params['encoding'])
-    print('成功处理文件: ' + params['in'])
-    print('处理后的文件为: ' + params['out'])
+    change_md_image()
+    print('成功处理文件: ' + arg.in_file)
+    print('处理后的文件为: ' + arg.out_file)
---
+++ src/Args.py
@@ -0,0 +1,79 @@
+"""
+命令行读取
+@author hujing
+"""
+import getopt
+import sys
+
+
+class Args:
+    """
+    用于处理全局参数
+    """
+
+    def __init__(self):
+        self.show_help = False
+        self.in_file = None
+        self.out_file = None
+        self.use_id = False
+        self.encoding = 'utf-8'
+        tmp, args = getopt.getopt(sys.argv[1:], 'i:o:d:e:h', ['encoding=', 'out=', 'in=', 'id=', 'help'])
+        # 读取参数
+        for comm in tmp:
+            # 帮助文档
+            if comm[0] == '-h' or comm[0] == '--help':
+                self.show_help = True
+            elif comm[0] == '-i' or comm[0] == '--in':
+                self.in_file = comm[1]
+            elif comm[0] == '-o' or comm[0] == '--out':
+                self.out_file = comm[1]
+            elif comm[0] == '-d' or comm[0] == '--id':
+                self.use_id = comm[1]
+            elif comm[0] == '-e' or comm[0] == '--encoding':
+                self.encoding = comm[1]
+        # 设置默认参数
+        self.out_file = self.out_file if self.out_file else str(self.in_file) + '2.md'
+
+    def need_print_help(self) -> bool:
+        """
+        检查是否需要输出帮助文档
+        :return:
+        """
+        if self.show_help:
+            return True
+        if not self.in_file or not self.out_file:
+            return True
+        return False
+
+    @staticmethod
+    def print_help() -> None:
+        """
+        输出帮助文档
+        """
+        print("""
+参数如下: 
+    -h/--help: 显示帮助文档
+    -i/--in: md文件路径, 必填
+    -o/--out: md输出文件路径, 默认 原文件名.md2.md
+    -d/--id: 是否使用id表示, 默认不适用, 0(不使用),1(使用)
+    -e/--encoding: 文件编码, 默认utf-8
+        制作者: 靖哥哥
+        """)
+        exit()
+
+    @staticmethod
+    def check_version():
+        """
+        检查python版本号
+        """
+        # Python2显示升级提示
+        if sys.version < '3':
+            print("""
+抱歉, 暂不支持Python2
+    请升级Python版本后重试
+                """)
+            exit()
+
+
+# 实例化, 保证单例
+arg = Args()
